74dfa7a6a2f513e2ca23d52b79214bb408381ae0
Kingsley Victor <javaprodigy56@gmail.com>
2021-11-07 22:43:34 +0100

    new commit

--- src/strings.js
+++ src/strings.js
@@ -1,9 +1,9 @@
-module.exports.HELP = `Welcome to VEFI Bot. Your bot companion
+module.exports.HELP = `Welcome VEFI Airdrop Bot. Your bot companion
 
 Available commands:
 
-/help - Shows help
-/address <addr (0x...)> - Associates your Binance address with your Telegram ID in this channel
-/balance - Checks the VEF balance of your associated address
-/updateAddress <addr (0x...)> - Updates your associated Binance address. This would only work if you have an associated Binance address
+/help - Shows help \n
+/address <addr (0x...)> - Associates your Binance address with your Telegram ID in this channel \n
+/balance - Checks the VEF balance of your associated address \n
+/updateAddress <addr (0x...)> - Updates your associated Binance address. This would only work if you have an associated Binance address \n
 `;
